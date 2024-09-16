7b4913823d02023a4dc691dcddf275d5ee19ac60
Alia Taher <alia.taher022@gmail.com>
2024-09-16 13:12:46 +0200

    add quote and fixed layout

--- src/pages/helpers.js
+++ src/pages/helpers.js
@@ -3,3 +3,24 @@
 export const isOneWord = (word)=>{
 return /^\S+$/.test(word)
 }
+
+
+
+export const reloadOnclick = ()=>{
+
+const leftTitle  = document.getElementById('title-left');
+const rightTitle = document.getElementById('title-right');
+
+leftTitle.href = '/'
+rightTitle.href = '/'
+
+leftTitle.onclick = (event)=>{
+    event.preventDefault();
+    window.location.reload();
+};
+
+rightTitle.onclick =(event )=>{
+    event.preventDefault();
+    window.location.reload();
+}
+}
--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -2,12 +2,15 @@ import {
   USER_INTERFACE_ID,
   SEARCH_BUTTON_ID,
   SEARCH_INPUT_ID,
+  QUOTE_ID,
 } from "../constants.js";
 import { createSearchElement } from "../views/mainView.js";
 import { initResultsPage } from "./resultsPage.js";
 import { isOneWord } from "./helpers.js";
+import { reloadOnclick } from "./helpers.js";
 
-export const initMainPage = () => {
+export const initMainPage = async () => {
+  reloadOnclick();
   const userInterface = document.getElementById(USER_INTERFACE_ID);
 
   userInterface.appendChild(createSearchElement());
@@ -25,14 +28,54 @@ export const initMainPage = () => {
   });
 
   searchBar.addEventListener("keydown", (event) => {
-    if(event.key === "Enter"){
+    if (event.key === "Enter") {
       const word = searchBar.value;
-    if (isOneWord(word) && event.key === "Enter") {
-      initResultsPage(word);
-    } else {
-      alert("Enter one word only");
+      if (isOneWord(word) && event.key === "Enter") {
+        initResultsPage(word);
+      } else {
+        alert("Enter one word only");
+      }
     }
-    }
-    
   });
+
+  /*
+   *   Generate a quote
+   */
+
+  const url = "https://dummyjson.com/quotes/random";
+
+  const fetchQuote = async () => {
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      console.log(result);
+      return result;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const renderQuote = async (data) => {
+    const displayQuote = document.createElement("div");
+    displayQuote.id = QUOTE_ID;
+
+    const { author, quote } = data;
+
+    displayQuote.innerHTML = String.raw`
+      <h6>Words to seek inspiration</h6>
+      <p>${quote}</p>
+      <p id = 'author'> ${author}</p>
+      `;
+    userInterface.appendChild(displayQuote);
+  };
+
+  try {
+     const quote = await fetchQuote(url);
+    await renderQuote(quote);
+  } catch (error) {
+    const displayError= document.createElement('div');
+    displayError.id = 'quote-error'
+    displayError.textContent='Something went wrong :( ' 
+    userInterface.appendChild(displayError)
+  }
 };
